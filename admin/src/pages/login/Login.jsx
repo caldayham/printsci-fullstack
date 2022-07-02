3dd3f43d74c157ec14a559775d0b00f386a224c5
cal <caldayham@gmail.com>
2022-07-02 11:57:29 -0500

    updating package in admin side

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 
 const Login = () => {
-  const [user, setUser] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <div>
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
+      <input
+        type="text"
+        placeholder="username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button>Login</button>
     </div>
   );
