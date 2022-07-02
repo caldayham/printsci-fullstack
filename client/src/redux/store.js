5c7b2f1c0239a79fdedcfe6351063c4922190c77
cal <caldayham@gmail.com>
2022-07-01 19:59:43 -0500

    starting to work on redux persisting state

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,6 +4,26 @@ import currentPageReducer from "./currentPageRedux";
 import overlayRedux from "./overlayRedux";
 import userReducer from "./userRedux";
 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, userReducer)
+
 export default configureStore({
     reducer: {
         cart: cartReducer,
