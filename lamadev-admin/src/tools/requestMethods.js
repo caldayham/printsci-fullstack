dab9cc828ef6555c41235452953a5f8e893206e1
cal <caldayham@gmail.com>
2022-07-03 09:55:14 -0500

    fixing non-tracking of lamadev-admin panel

---
+++ lamadev-admin/src/tools/requestMethods.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:5000/api/"
+var TOKEN = ""
+if (JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser !== null) {
+    TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken
+}
+
+export const publicRequest = axios.create({
+    baseURL: BASE_URL,
+});
+
+export const userRequest = axios.create({
+    baseURL: BASE_URL,
+    header: { token: `Bearer ${TOKEN}` },
+});
